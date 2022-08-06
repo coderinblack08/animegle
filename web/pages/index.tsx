daf62a7ea86143ce84c871258903fd075041fe88
coderinblack08 <kevinlu.email@gmail.com>
2022-08-06 00:43:14 -0700

    feat: match page

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,7 +1,15 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <div className="max-w-3xl mx-auto px-5 py-12">
+      <h1 className="text-3xl font-bold">Animegle</h1>
+      <p className="text-gray-500 mb-6 mt-1">
+        Omegle but with your face is replaced with an anime girl/boy.
+      </p>
+      <button>Match me</button>
+    </div>
+  );
 };
 
 export default Home;
